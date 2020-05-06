55290ad0a029b5bf4419933af899501f79ab5568
hiromu <htakahashi262@gmail.com>
2020-05-06 23:55:39 +0900

    レスポンシブ対応　トップページ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   # process :resize_to_limit => [400, 400, "#ffffff""Center"]
-  process :resize_and_pad => [500, 500, "#ffffff", "Center"]
+  # process :resize_and_pad => [800, 800, "#ffffff", "Center"]
+  process :resize_and_pad => [800, 1000]
   # process resize_to_fit: [300, 200]
   # process resize_to_fill: [100, 100, "Center"]
   # Choose what kind of storage to use for this uploader:
